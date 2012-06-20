32392253e74b600a3274fb4b37a4532bf91b78e5
Scott Stults <sstults@opensourceconnections.com>
2012-06-20 06:15:46 -0400

    woops, we're not looping over the arguments anymore

--- single_load.sh
+++ single_load.sh
@@ -22,41 +22,38 @@ url=$1
 data_dir=$2
 
 
-for url in $@
-do
-    file=`echo ${url} | awk -F '/' '{print $7}'`
-    filebase=`echo ${file} | awk -F '.' '{print $1}'`
-    mkdir $filebase
-    ln -s ${SCRIPT_DIR}/categories.xml ${filebase}/categories.xml
-    ln -s ${SCRIPT_DIR}/saxon9he.jar ${filebase}/saxon9he.jar
-    ln -s ${SCRIPT_DIR}/convert.xsl ${filebase}/convert.xsl
-    ln -s ${SCRIPT_DIR}/cals_table.xsl ${filebase}/cals_table.xsl
-    (
-    cd $filebase
-    wget -q ${url} -o wget.log
-    ${SCRIPT_DIR}/fix-zip-filenames.sh
-    #echo ${file}
-    unzip ${file}
-    ${SCRIPT_DIR}/convert.sh ${filebase}.xml ${filebase}.json
-    
-    
-    CURL="http://localhost:8983/solr/admin/cores?action=CREATE"
-    IDIR="instanceDir=/home/ec2-user/solr/dir_search_cores/us_patent_grant_v2_0/"
-    CFILE="config=solrconfig.xml"
-    SFILE="schema=schema.xml"
-    DDIR="dataDir=${data_dir}"
-    curl "${CURL}&name=${filebase}&${IDIR}&${CFILE}&${SFILE}&${DDIR}"
-    
-    
-    
-    (SOLR_CORE=${filebase}; ${SCRIPT_DIR}/post_json.sh ${file}.json > /dev/null)
-    #####rm -f ${file}.json ${filebase}.xml ${file}
-    )
+file=`echo ${url} | awk -F '/' '{print $7}'`
+filebase=`echo ${file} | awk -F '.' '{print $1}'`
+mkdir $filebase
+ln -s ${SCRIPT_DIR}/categories.xml ${filebase}/categories.xml
+ln -s ${SCRIPT_DIR}/saxon9he.jar ${filebase}/saxon9he.jar
+ln -s ${SCRIPT_DIR}/convert.xsl ${filebase}/convert.xsl
+ln -s ${SCRIPT_DIR}/cals_table.xsl ${filebase}/cals_table.xsl
+(
+cd $filebase
+wget -q ${url} -o wget.log
+${SCRIPT_DIR}/fix-zip-filenames.sh
+#echo ${file}
+unzip ${file}
+${SCRIPT_DIR}/convert.sh ${filebase}.xml ${filebase}.json
 
-    END=$(date +%s)
-    DIFF=$(( $END - $START ))
-    echo "${file} Processed in $DIFF seconds"
-done
+
+CURL="http://localhost:8983/solr/admin/cores?action=CREATE"
+IDIR="instanceDir=/home/ec2-user/solr/dir_search_cores/us_patent_grant_v2_0/"
+CFILE="config=solrconfig.xml"
+SFILE="schema=schema.xml"
+DDIR="dataDir=${data_dir}"
+curl "${CURL}&name=${filebase}&${IDIR}&${CFILE}&${SFILE}&${DDIR}"
+
+
+
+(SOLR_CORE=${filebase}; ${SCRIPT_DIR}/post_json.sh ${file}.json > /dev/null)
+#####rm -f ${file}.json ${filebase}.xml ${file}
+)
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "${file} Processed in $DIFF seconds"
 
 END=$(date +%s)
 DIFF=$(( $END - $START ))
