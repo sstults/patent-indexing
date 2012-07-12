7fcb9fe2465b26615351bea10eb0011d557c0a6d
Scott Stults <sstults@opensourceconnections.com>
2012-07-11 23:42:46 -0400

    upgraded to v3, and made it so I can run it a bunch on a big server

--- single_load.sh
+++ single_load.sh
@@ -11,6 +11,7 @@ START=$(date +%s)
 EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
 SCRIPT_DIR=${SCRIPT_DIR:-~/patent-indexing}
 SOLR_XML=${SCRIPT_DIR}/solr/dir_search_cores/solr.xml
+CREATE_CORE=${CREATE_CORE:-true}
 
 # Unzip categories.zip if necessary
 if [ ! -f ${SCRIPT_DIR}/categories.xml ] ; then
@@ -29,6 +30,10 @@ ln -s ${SCRIPT_DIR}/categories.xml ${filebase}/categories.xml
 ln -s ${SCRIPT_DIR}/saxon9he.jar ${filebase}/saxon9he.jar
 ln -s ${SCRIPT_DIR}/convert.xsl ${filebase}/convert.xsl
 ln -s ${SCRIPT_DIR}/cals_table.xsl ${filebase}/cals_table.xsl
+ln -s ${SCRIPT_DIR}/cpc_2005_2010.xml ${filebase}/cpc_2005_2010.xml
+ln -s ${SCRIPT_DIR}/cpc_codes_2009.xml ${filebase}/cpc_codes_2009.xml
+ln -s ${SCRIPT_DIR}/cpc_facets.xml ${filebase}/cpc_facets.xml
+ln -s ${SCRIPT_DIR}/cpc_facets.xml ${filebase}/cpc_facets.xml
 (
     cd $filebase
     
@@ -48,19 +53,26 @@ ln -s ${SCRIPT_DIR}/cals_table.xsl ${filebase}/cals_table.xsl
         ${SCRIPT_DIR}/convert.sh ${filebase}.xml ${filebase}.json >> ~/${EC2_INSTANCE_ID}.${filebase}.convert.log 2>&1
     fi
     
+    INDEX_SIZE=`du -sh ${data_dir} | cut -f 1`
+
     # Create core if needed
-    if [ "1" != `grep -c ${filebase} $SOLR_XML` ]
+    if [ "true" = $CREATE_CORE ]
     then
-        CURL="http://localhost:8983/solr/admin/cores?action=CREATE"
-        IDIR="instanceDir=/home/ec2-user/patent-indexing/solr/dir_search_cores/us_patent_grant_v2_0/"
-        CFILE="config=solrconfig.xml"
-        SFILE="schema=schema.xml"
-        DDIR="dataDir=${data_dir}"
-        curl "${CURL}&name=${filebase}&${IDIR}&${CFILE}&${SFILE}&${DDIR}"
+        if [ "1" != `grep -c ${filebase} $SOLR_XML` ]
+        then
+            CURL="http://localhost:8983/solr/admin/cores?action=CREATE"
+            IDIR="instanceDir=/home/ec2-user/patent-indexing/solr/dir_search_cores/us_patent_grant_v3_0/"
+            CFILE="config=solrconfig.xml"
+            SFILE="schema=schema.xml"
+            DDIR="dataDir=${data_dir}"
+            curl "${CURL}&name=${filebase}&${IDIR}&${CFILE}&${SFILE}&${DDIR}"
+        fi
+        (export SOLR_CORE=${filebase}; ${SCRIPT_DIR}/post_json.sh ${filebase}.json >> ~/${EC2_INSTANCE_ID}.${filebase}.post.log 2>&1)
+    else
+        (export SOLR_CORE=us_patent_grant; ${SCRIPT_DIR}/post_json.sh ${filebase}.json >> ~/${EC2_INSTANCE_ID}.${filebase}.post.log 2>&1)
+
     fi
     
-    INDEX_SIZE=`du -sh ${data_dir} | cut -f 1`
-    (export SOLR_CORE=${filebase}; ${SCRIPT_DIR}/post_json.sh ${filebase}.json >> ~/${EC2_INSTANCE_ID}.${filebase}.post.log 2>&1)
     #####rm -f ${file}.json ${filebase}.xml ${file}
 
     END=$(date +%s)
